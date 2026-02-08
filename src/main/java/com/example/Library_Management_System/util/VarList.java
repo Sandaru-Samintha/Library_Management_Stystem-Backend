f65a55a7348731da02c23968240ea36a90967f1c
Sandaru Samintha <sandarusaminthagunarathna@gmail.com>
2026-02-08 22:31:42 +0530

    Create the Book Service  in the library management system

--- src/main/java/com/example/Library_Management_System/util/VarList.java
+++ src/main/java/com/example/Library_Management_System/util/VarList.java
@@ -1,6 +1,7 @@
 package com.example.Library_Management_System.util;
 
-public class varList {
+public class VarList {
+
     public static final String RSP_SUCCESS          = "00";
     public  static final String RSP_NO_DATA_FOUND   = "01";
     public static  final String RSP_NOT_AUTHORISED  = "02";
