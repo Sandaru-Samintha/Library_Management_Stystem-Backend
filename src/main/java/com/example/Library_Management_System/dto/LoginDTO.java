8ceab0a364298ffa1a3ac433024ffe879c6967bc
Sandaru Samintha <sandarusaminthagunarathna@gmail.com>
2026-02-20 23:05:07 +0530

    create the login dto in library managment system

---
+++ src/main/java/com/example/Library_Management_System/dto/LoginDTO.java
@@ -0,0 +1,14 @@
+package com.example.Library_Management_System.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class LoginDTO {
+    private String email;
+    private String password;
+    private String role; // "MEMBER" or "ADMIN"
+}
